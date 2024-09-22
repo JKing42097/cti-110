1f2a466ab9cb4d26dfe2741af35d814aa393c81e
JKing42097 <kingj8744@student.faytechcc.edu>
2024-09-22 11:36:29 -0400

    Add files via upload

---
+++ P1LAB1_KingJustin.py
@@ -0,0 +1,13 @@
+# Justin King
+# September 22, 2024
+# P1LAB1
+# This program prompts the user for their first and last name, then displays a welcome message.
+
+# Prompt the user for their first name
+first_name = input("Please enter your first name: ")
+
+# Prompt the user for their last name
+last_name = input("Please enter your last name: ")
+
+# Display a welcome message
+print(f"Welcome to Web, Pgm, & DB foundations, {first_name} {last_name}!")
